143614b1265239f92962e8a4ebbe813e82bb6dcc
Sinevia <info@sinevia.com>
2018-07-30 03:30:05 +0100

    Update 2018_01_01_000001_package_sinevia_tasks_tables_create.php

--- database/migrations/2018_01_01_000001_package_sinevia_tasks_tables_create.php
+++ database/migrations/2018_01_01_000001_package_sinevia_tasks_tables_create.php
@@ -6,28 +6,17 @@ class PackageSineviaTasksTablesCreate extends Illuminate\Database\Migrations\Mig
      * @return void
      */
     public function up() {
-        Sinevia\Cms\Models\Template::tableCreate();
-        Sinevia\Cms\Models\TemplateTranslation::tableCreate();
-        Sinevia\Cms\Models\Page::tableCreate();
-        Sinevia\Cms\Models\PageTranslation::tableCreate();
-        Sinevia\Cms\Models\Block::tableCreate();
-        Sinevia\Cms\Models\BlockTranslation::tableCreate();
-        Sinevia\Cms\Models\Widget::tableCreate();
-        Sinevia\Cms\Models\Version::tableCreate();
+        Sinevia\Tasks\Models\Task::tableCreate();
+        Sinevia\Tasks\Models\Queue::tableCreate();
     }
+    
     /**
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down() {
-        Sinevia\Cms\Models\Template::tableDelete();
-        Sinevia\Cms\Models\TemplateTranslation::tableDelete();
-        Sinevia\Cms\Models\Page::tableDelete();
-        Sinevia\Cms\Models\PageTranslation::tableDelete();
-        Sinevia\Cms\Models\Block::tableDelete();
-        Sinevia\Cms\Models\BlockTranslation::tableDelete();
-        Sinevia\Cms\Models\Widget::tableDelete();
-        Sinevia\Cms\Models\Version::tableDelete();
+    public function down() {        
+        Sinevia\Tasks\Models\Queue::tableDelete();
+        Sinevia\Tasks\Models\Task::tableDelete();
     }
 }
