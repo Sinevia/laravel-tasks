447b9f8a9b98967da380dc10bc4819f0edc9bec3
Sinevia <info@sinevia.com>
2018-07-30 03:24:12 +0100

    Create TasksServiceProvider.php

---
+++ src/TasksServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Sinevia\Tasks;
+
+use Illuminate\Support\ServiceProvider;
+
+class TasksServiceProvider extends ServiceProvider {
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot() {
+        $this->publishes([
+            dirname(__DIR__) . '/config/cms.php' => config_path('cms.php'),
+            $this->loadMigrationsFrom(dirname(__DIR__) . '/database/migrations'),
+            //$this->loadViewsFrom(dirname(__DIR__) . '/resources/views', 'tasks'),
+            //$this->loadRoutesFrom(dirname(__DIR__).'/routes.php'),            
+        ]);
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register() {
+        //
+    }
+
+}
