5c86f2ee3f068b83fb187bd6132c8b33fa11969e
Sinevia <info@sinevia.com>
2019-04-26 19:51:59 +0100

    Update TasksServiceProvider.php

--- src/TasksServiceProvider.php
+++ src/TasksServiceProvider.php
@@ -14,10 +14,10 @@ class TasksServiceProvider extends ServiceProvider {
     public function boot() {
         $this->publishes([
             dirname(__DIR__) . '/config/tasks.php' => config_path('tasks.php'),
-            $this->loadMigrationsFrom(dirname(__DIR__) . '/database/migrations'),
-            //$this->loadViewsFrom(dirname(__DIR__) . '/resources/views', 'tasks'),
-            //$this->loadRoutesFrom(dirname(__DIR__).'/routes.php'),            
         ]);
+        $this->loadMigrationsFrom(dirname(__DIR__) . '/database/migrations'),
+        // $this->loadViewsFrom(dirname(__DIR__) . '/resources/views', 'tasks'),
+        // $this->loadRoutesFrom(dirname(__DIR__).'/routes.php'),            
     }
 
     /**
