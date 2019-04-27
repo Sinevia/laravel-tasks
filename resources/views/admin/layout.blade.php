97ebebf66d80de9c10902ba0105d310cd4507772
Sinevia <info@sinevia.com>
2019-04-27 20:30:15 +0100

    Update layout.blade.php

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -8,7 +8,7 @@
         <meta name="author" content="Sinevia Ltd">
         <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="favicon.ico" />
         <link rel="icon" type="image/png" href="favicon.png" />
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
         <script src="//code.jquery.com/jquery-3.4.0.min.js"></script>
         <link href="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" />
         <script src="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
