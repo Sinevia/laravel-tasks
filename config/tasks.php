dbe702de07ba2b8f5c24db8b1dd4e1ea8d085d15
Sinevia <info@sinevia.com>
2019-04-27 07:27:05 +0100

    Update tasks.php

--- config/tasks.php
+++ config/tasks.php
@@ -5,4 +5,8 @@
  * Set specific configuration variables here
  */
 return [
+    // automatic loading of routes through main service provider
+    'routes' => true,
+    // layout where the task pages will show into, i.e. admin.layouts.master
+    'layout-master' => 'tasks::admin.layout',
 ];
