0db14c4f3e3047643293332b929a4eafe7f1f4c2
Sinevia <info@sinevia.com>
2019-04-27 21:47:43 +0100

    Update shared-navigation.blade.php

--- resources/views/admin/shared-navigation.blade.php
+++ resources/views/admin/shared-navigation.blade.php
@@ -1,1 +1,17 @@
-<?php
+<ul class="nav nav-pills nav-fill bg-light" style="margin: 0px 0px 10px 0px;">
+    <!--
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Dashboard</a>
+    </li>
+    -->
+    <li class="nav-item">
+        <a class="nav-link" href="<?php echo Sinevia\Tasks\Helpers\Links::adminQueueManager(); ?>">
+            Queue
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="<?php echo Sinevia\Tasks\Helpers\Links::adminTaskManager(); ?>">
+            Tasks
+        </a>
+    </li>
+</ul>
