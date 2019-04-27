745c19b6dca6194665c099803f56e09445e0ae65
Sinevia <info@sinevia.com>
2019-04-27 06:58:13 +0100

    Create TasksController.php

---
+++ src/Http/Controllers/TasksController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sinevia\Tasks\Http\Controllers;
+/**
+ * Contains simple Task management functionality
+ */
+class TasksController extends \Illuminate\Routing\Controller {
+    function anyIndex(){
+        return 'Working';
+    }
+}
+
