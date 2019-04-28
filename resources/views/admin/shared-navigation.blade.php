c82362a844799369e07dc1902b5d7ebc6a7ff1e8
Sinevia <info@sinevia.com>
2019-04-28 06:22:32 +0100

    Update shared-navigation.blade.php

--- resources/views/admin/shared-navigation.blade.php
+++ resources/views/admin/shared-navigation.blade.php
@@ -4,18 +4,21 @@ $taskCount = \Sinevia\Tasks\Models\Task::where('Status', '<>', 'Deleted')->count
 ?>
 <ul class="nav nav-pills nav-fill bg-light" style="margin: 0px 0px 10px 0px;">
     <li class="nav-item">
-        <a class="nav-link active" href="<?php echo Sinevia\Tasks\Helpers\Links::adminHome(); ?>">Dashboard</a>
+        <?php $active = request()->fullUrl() == Sinevia\Tasks\Helpers\Links::adminHome() ? ' active' : ''; ?>
+        <a class="nav-link {{ $active }}" href="<?php echo Sinevia\Tasks\Helpers\Links::adminHome(); ?>">Dashboard</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="<?php echo Sinevia\Tasks\Helpers\Links::adminQueueManager(); ?>">
+        <?php $active = request()->fullUrl() == Sinevia\Tasks\Helpers\Links::adminQueueManager() ? ' active' : ''; ?>
+        <a class="nav-link {{ $active }}" href="<?php echo Sinevia\Tasks\Helpers\Links::adminQueueManager(); ?>">
             Queue
             <span class="badge badge-secondary""><?php echo $queuedCount; ?></span>
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="<?php echo Sinevia\Tasks\Helpers\Links::adminTaskManager(); ?>">
+        <?php $active = request()->fullUrl() == Sinevia\Tasks\Helpers\Links::adminTaskManager() ? ' active' : ''; ?>
+        <a class="nav-link {{ $active }}" href="<?php echo Sinevia\Tasks\Helpers\Links::adminTaskManager(); ?>">
             Tasks
-            <span class="badge badge-secondary""><?php echo $taskCount; ?></span>
+            <span class="badge badge-secondary"><?php echo $taskCount; ?></span>
         </a>
     </li>
 </ul>
