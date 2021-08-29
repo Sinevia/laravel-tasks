1be9c8916ea38e0be49d37958a5a000025b248d5
Milan Lesichkov <lesichkovm@gmail.com>
2021-08-29 08:28:24 +0100

    Update Links.php

--- src/Helpers/Links.php
+++ src/Helpers/Links.php
@@ -15,6 +15,10 @@ class Links {
     public static function adminTaskManager($queryData = []) {
         return action('\Sinevia\Tasks\Http\Controllers\TasksController@getTaskManager') . self::buildQueryString($queryData);
     }
+    
+    public static function fetchTasksAjax($queryData = []) {
+        return action('\Sinevia\Tasks\Http\Controllers\TasksController@anyQueueTasksFetchAjax') . self::buildQueryString($queryData);
+    }
 
     private static function buildQueryString($queryData = []) {
         $queryString = '';
