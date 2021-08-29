e02d54068445f3fb8f50e675553bd486ae55d7da
Milan Lesichkov <lesichkovm@gmail.com>
2021-08-29 08:18:30 +0100

    Update queue-task-parameters-modal.blade.php

--- resources/views/admin/queue-task-parameters-modal.blade.php
+++ resources/views/admin/queue-task-parameters-modal.blade.php
@@ -1,16 +1,5 @@
 <!-- START: Task Parameters Dialog -->
-<style>
-    #ModalTaskParameters sup{
-        font-size:11px;
-    }
-    #ModalTaskParameters .modal-footer{
-        display: table;
-    }
-    #ModalTaskParameters .modal-lg {
-        max-width: 80% !important;
-    }
-</style>
-<div id="ModalTaskParameters" class="modal fade" style="display:none;">
+<div id="ModalTaskParameters" class="modal fade" style="display:none;/*max-width: 80% !important;*/">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
@@ -25,30 +14,4 @@
         </div>
     </div>
 </div>
-
-<script>
-    function showTaskParametersModal(taskId) {
-        var url = '<?php echo action('\Sinevia\Tasks\Http\Controllers\TasksController@anyQueueTaskParametersAjax'); ?>?QueuedTaskId=' + taskId;
-        $.ajax({// ajax call starts
-            url: url,
-            data: {QueuedTaskId: taskId, _token: "<?php echo csrf_token(); ?>"},
-            dataType: 'json'
-        }).done(function (response) {
-            // DEBUG: console.log(response)
-            if (response.status === 'success') {
-                $('#ModalTaskParameters textarea[name=Parameters]').val(response.data.Parameters);
-            } else {
-                alert(response.message);
-                $('#ModalTaskParameters').modal('hide');
-            }
-        }).fail(function () {
-            alert('Getting details failed');
-            $('#ModalTaskParameters').modal('hide');
-        });
-        
-        $('#ModalTaskParameters textarea').val('Loading details for task ref. ' + taskId + '...');
-        $('#ModalTaskParameters').modal('show');
-        
-    }
-</script>
 <!-- END: Task Parameters Dialog -->
