eb9c584e251e0a2a77fae29715e5991ec9215595
Milan Lesichkov <lesichkovm@gmail.com>
2021-08-29 08:18:55 +0100

    Update queue-task-requeue-modal.blade.php

--- resources/views/admin/queue-task-requeue-modal.blade.php
+++ resources/views/admin/queue-task-requeue-modal.blade.php
@@ -1,13 +1,4 @@
 <!-- START: Task Requeue Dialog -->
-<style>
-    #ModalTaskRequeue sup{
-        font-size:11px;
-    }
-    #ModalTaskRequeue .modal-footer{
-        display: table;
-        width:100%;
-    }
-</style>
 <div id="ModalTaskRequeue" class="modal fade" style="display:none;">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -24,12 +15,12 @@
                 </div>
                 <input type="hidden" name="QueuedTaskId"></textarea>
             </div>
-            <div class="modal-footer">
+            <div class="modal-footer" style="display:table;width: 100%;">
                 <a id="modal-close" href="#" class="btn btn-info float-left" data-dismiss="modal">
                     <i class="fas fa-chevron-left"></i>
                     Cancel
                 </a>
-                <a id="modal-close" href="#" class="btn btn-success float-right" data-dismiss="modal" onclick="taskRequeue();">
+                <a id="modal-close" href="#" class="btn btn-success float-right" data-dismiss="modal" v-on:click="taskRequeue();">
                     <i class="fas fa-check-circle"></i>
                     Continue
                 </a>
@@ -37,56 +28,4 @@
         </div>
     </div>
 </div>
-
-<script>
-
-    function showTaskRequeueModal(taskId) {
-        $('#ModalTaskRequeue input[name=QueuedTaskId]').val(taskId);
-                
-        window.autoreload = false;
-        var url = '<?php echo action('\Sinevia\Tasks\Http\Controllers\TasksController@anyQueueTaskParametersAjax'); ?>?QueuedTaskId=' + taskId;
-        $.ajax({// ajax call starts
-            url: url,
-            data: {QueuedTaskId: taskId, _token: "<?php echo csrf_token(); ?>"},
-            dataType: 'json'
-        }).done(function (response) {
-            // DEBUG: console.log(response)
-            if (response.status === 'success') {
-                $('#ModalTaskRequeue textarea[name=Parameters]').val(response.data.Parameters);
-                $('#ModalTaskRequeue').modal('show');
-            } else {
-                alert(response.message);
-                $('#ModalTaskRequeue').modal('hide');
-            }
-        }).fail(function () {
-            alert('Getting parameters failed');
-        });
-    }
-
-    function taskRequeue() {
-        var taskId = $('#ModalTaskRequeue input[name=QueuedTaskId]').val();
-        var parameters = $('#ModalTaskRequeue textarea[name=Parameters]').val();
-
-        var url = '<?php echo action('\Sinevia\Tasks\Http\Controllers\TasksController@anyQueueTaskRequeueAjax'); ?>?QueuedTaskId=' + taskId;
-        $.ajax({// ajax call starts
-            url: url,
-            data: {QueuedTaskId: taskId, Parameters: parameters, _token: "<?php echo csrf_token(); ?>"},
-            dataType: 'json'
-        }).done(function (response) {
-            // DEBUG: console.log(response)
-            if (response.status === 'success') {
-                $('#ModalTaskRequeue').modal('hide');
-                window.location.href = window.location.href;
-            } else {
-                alert(response.message);
-                $('#ModalTaskRequeue').modal('hide');
-                window.location.href = window.location.href;
-            }
-        }).fail(function () {
-            alert('Getting details failed');
-            $('#ModalTaskRequeue').modal('hide');
-        });
-        window.autoreload = true;
-    }
-</script>
 <!-- END: Task Requeue Dialog -->
